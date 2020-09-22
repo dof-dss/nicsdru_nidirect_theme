9475f0b7f5b7aedddde8d701796d05c33bf37209
omahm <hello@onemanandhismouse.com>
2020-09-22 11:07:12 +0100

    D8NID-834 ~ Hide links on init. Code comments

--- src/js/ui.js
+++ src/js/ui.js
@@ -6,19 +6,26 @@
 
 /* eslint-disable */
 /*
-  Toggle display of multiple links on card blocks.
+ * User Interface scripts.
  */
 (function ($, Drupal) {
   Drupal.behaviors.nidirectui = {
     attach: function attach (context) {
 
+      // Toggles the display of links within a card with multiple
+      // field_link values.
       $('.card--multilink', context).once('nicsdruAccordions').each(function(index) {
-        var links_wrapper = $(this).find('.card__links');
+        const links_wrapper = $(this).find('.card__links');
 
+        // Show/hide event handler.
         $(this).on('click', function () {
           $(links_wrapper).toggle();
         })
 
+        // Initially hide the links.
+        $(links_wrapper).hide();
+
+        // Prevent link click event from triggering parent show/hide handler.
         $(links_wrapper).find('a').click(function(e) {
           e.stopPropagation();
         });
