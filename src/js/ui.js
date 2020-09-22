46aaa2b69699cc67b177f0f3c2c27604c72d3fe7
omahm <hello@onemanandhismouse.com>
2020-09-22 10:08:30 +0100

    D8NID-834 ~ Add multilink support to card

--- src/js/ui.js
+++ src/js/ui.js
@@ -11,7 +11,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.nidirectui = {
     attach: function attach (context) {
-      
+
       $('.card--multilink', context).once('nicsdruAccordions').each(function(index) {
         $(this).on('click', function () {
           $(this).find('.card__links').toggle();
