3f97c24ba2ea3c473c27e331dea6f0feac43bec9
Neil Blair <neil.r.blair@gmail.com>
2021-07-02 20:57:03 +0100

    Refactor thwooper js

--- src/js/thwoop-images.js
+++ src/js/thwoop-images.js
@@ -15,52 +15,53 @@
 
       // Add a clickable/focusable wrapper and icon to indicate image is thwoopable.
       $thwoopImages.once('thwoop-toggle').each(function () {
-        $(this).wrap('<a class="thwooper" href="#"></a>');
-      });
 
-      // Click or keypress toggles thwoop.
-      $('.thwooper').click(function (event) {
-        event.preventDefault();
-        var $thwoopimage = $(this).find('img, figure');
-        var $thwoop_wrap = $(this).closest('.media-image');
-        var thwoop_picture_mapping = $(this).parent().attr('data-picture-mapping');
-        var modal = false;
+        var $thwooper = $('<a class="thwooper" href="#"></a>');
 
-        if (thwoop_picture_mapping == 'inline_xl_expandable' || thwoop_picture_mapping == 'portrait_full_expandable') {
-          modal = true;
+        $thwooper.click(function (event) {
+          event.preventDefault();
 
-          // Enable escape key to close modal.
-          $(document).keydown(function(event) {
-            if (event.keyCode == 27) {
+          var $thwoopimage = $(this).find('img, figure');
+          var $thwoop_wrap = $(this).closest('.media-image');
+          var thwoop_picture_mapping = $(this).parent().attr('data-picture-mapping');
+          var modal = (thwoop_picture_mapping == 'inline_xl_expandable' || thwoop_picture_mapping == 'portrait_full_expandable');
+
+          if ($(this).hasClass('thwooped')) {
+            $(this).removeClass('thwooped');
+            $(this).attr('aria-label', 'Enlarge');
+            if (modal) {
               $thwoop_wrap.removeClass('thwooped-modal');
-              $thwoopimage.removeClass('thwooped');
               // Just in case user scrolled page behind the modal, bring image back into view.
               $thwoop_wrap[0].scrollIntoView({
                 block: 'center',
               });
             }
-          });
-        }
+          } else {
+            $(this).addClass('thwooped');
+            if (modal) {
+              $thwoop_wrap.addClass('thwooped-modal');
+              $(this).attr('aria-label', 'Close');
+            } else {
+              $(this).attr('aria-label', 'Shrink');
+            }
+          }
 
-        if ($(this).hasClass('thwooped')) {
-          $(this).removeClass('thwooped');
-          $(this).attr('aria-label', 'Enlarge');
+          // Enable escape key if showing image in a modal.
           if (modal) {
-            $thwoop_wrap.removeClass('thwooped-modal');
-            // Just in case user scrolled page behind the modal, bring image back into view.
-            $thwoop_wrap[0].scrollIntoView({
-              block: 'center',
+            $(document).keydown(function(event) {
+              if (event.keyCode == 27) {
+                $thwoop_wrap.removeClass('thwooped-modal');
+                $thwoopimage.removeClass('thwooped');
+                // Just in case user scrolled page behind the modal, bring image back into view.
+                $thwoop_wrap[0].scrollIntoView({
+                  block: 'center',
+                });
+              }
             });
           }
-        } else {
-          $(this).addClass('thwooped');
-          if (modal) {
-            $thwoop_wrap.addClass('thwooped-modal');
-            $(this).attr('aria-label', 'Close');
-          } else {
-            $(this).attr('aria-label', 'Shrink');
-          }
-        }
+        });
+
+        $(this).wrap($thwooper);
       });
     }
   };
